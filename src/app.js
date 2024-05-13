ccd9eb9c972d366ed9b570d53463a9bcd3aa4b48
Gabriel Luiz <gabriel.araujo@ubus.com.br>
2024-05-13 10:52:43 -0300

    correção de cors

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express"
 import {funcionarioController} from "./controllers/funcionarioController.js"
 import bodyParser from "body-parser";
+import cors from 'cors'
 const app = express();
 
+app.use(cors());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- src/useCases/getFuncionariosById.js
+++ src/useCases/getFuncionariosById.js
@@ -1,18 +1,65 @@
 import {sql} from "../libs/postgres.js"
 
 export const getFuncionariosById = async (req, res) => {
-    const {id} = req.params;
+    const request = req.params;
 
     try {
         const dbFuncionario = await sql`SELECT *
                                         FROM funcionarios
-                                        WHERE id = ${id}`
+                                        WHERE id = ${request.id}`
+
+        const {
+            id,
+            chapa,
+            cnhnumero,
+            dtinativacao,
+            dtnascimento,
+            dtregistro,
+            dtvenccnh,
+            dtvenccredencial,
+            idbairro,
+            idcidade,
+            idempresa,
+            idfuncao,
+            idgaragem,
+            nome,
+            numero,
+            obs,
+            rua,
+            status,
+            telefoneprincipal,
+            telefonesecundario} = dbFuncionario[0]
 
 
-        res.json({dados:dbFuncionario});
+        const newObj = {
+            id: id,
+            nome: nome,
+            idEmpresa: Number(idempresa),
+            idFuncao: Number(idfuncao),
+            dtRegistro: new Date(dtregistro),
+            chapa: chapa,
+            status: status,
+            telefonePrincipal: telefoneprincipal,
+            telefoneSecundario: telefonesecundario,
+            idBairro: Number(idbairro),
+            rua: rua,
+            idCidade: Number(idcidade),
+            numero: numero,
+            dtNascimento: dtnascimento,
+            CNHNumero: cnhnumero,
+            dtVenCNH: dtvenccnh,
+            dtVenCredencial: dtvenccredencial,
+            dtInativacao: dtinativacao,
+            idGaragem: Number(idgaragem),
+            obs: obs
+           
+        }
+
+        res.json({dados:newObj});
     } catch (err) {
         res.send('Não foi possível resgatar o funcionário');
     }
 }
+
 
 
